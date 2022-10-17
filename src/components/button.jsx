672da699f3701b55cb90b103941dbd259a4e1045
Artemy <artegoser@gmail.com>
2022-10-17 17:50:44 +0300

    feat: img animation

--- src/components/button.jsx
+++ src/components/button.jsx
@@ -4,7 +4,7 @@ function Button(props) {
   return (
     <Link to={props.href} className={props.className}>
       <div
-        className={`transition-transform w-48 ease-[cubic-bezier(.69,.58,.32,1.69)] delay-60 hover:scale-105  p-2 pl-6 text-lg bg-zinc-100 hover:bg-zinc-300 dark:bg-zinc-600 dark:hover:bg-zinc-800 rounded-2xl ${props.className}`}
+        className={`transition-transform w-48 ease-[cubic-bezier(.69,.58,.32,1.69)] hover:scale-105  p-2 pl-6 text-lg bg-zinc-100 hover:bg-zinc-300 dark:bg-zinc-600 dark:hover:bg-zinc-800 rounded-2xl ${props.className}`}
       >
         {props.children}
       </div>
@@ -16,7 +16,7 @@ function ButtonWithAction(props) {
   return (
     <div
       onClick={props.onClick}
-      className={`transition-transform w-48 ease-[cubic-bezier(.69,.58,.32,1.69)] delay-60 hover:scale-105  p-2 pl-6 text-lg bg-zinc-100 hover:bg-zinc-300 dark:bg-zinc-600 dark:hover:bg-zinc-800 rounded-2xl ${props.className}`}
+      className={`transition-transform w-48 ease-[cubic-bezier(.69,.58,.32,1.69)] hover:scale-105  p-2 pl-6 text-lg bg-zinc-100 hover:bg-zinc-300 dark:bg-zinc-600 dark:hover:bg-zinc-800 rounded-2xl ${props.className}`}
     >
       {props.children}
     </div>
