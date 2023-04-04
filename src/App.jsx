112db5c6ac2327888489338674fdb2bba16739ed
Artemy <artegoser@gmail.com>
2023-04-04 18:15:46 +0300

    fix: Canceling the chat

--- src/App.jsx
+++ src/App.jsx
@@ -37,6 +37,8 @@ function App() {
     setKey(Math.random());
   });
 
+  window.socket = socket;
+
   return (
     <div
       className="grid grid-cols-4  lg:grid-cols-5  gap-10 text-black dark:text-white"
--- src/pages/chat.jsx
+++
@@ -1,3 +0,0 @@
-function Chat() {}
-
-export default Chat;
