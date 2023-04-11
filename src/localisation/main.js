8737ebc3adab91617955e19ebf1bb4871ab303d5
Artemy <artegoser@gmail.com>
2023-04-11 11:30:11 +0300

    feat: some asian languages

--- src/localisation/main.js
+++ src/localisation/main.js
@@ -2,12 +2,16 @@ import ru from "./ru";
 import en from "./en";
 import es from "./es";
 import eo from "./eo";
+import ja from "./ja";
+import zh from "./zh";
 
 let Locales = {
   ru,
   en,
   es,
   eo,
+  ja,
+  zh,
 };
 
 let langChoices = [
@@ -26,6 +30,14 @@ let langChoices = [
   {
     value: "eo",
     label: "Esperanto",
+  },
+  {
+    value: "ja-JP",
+    label: "日本語",
+  },
+  {
+    value: "zh-CN",
+    label: "中国人",
   },
 ];
 
