701a97d7e186159bf8b38379790429238397408b
Artemy <artegoser@gmail.com>
2023-04-21 13:05:31 +0300

    doc: update license

--- src/components/styles.js
+++ src/components/styles.js
@@ -1,3 +1,20 @@
+/*
+ Copyright (c) 2023 artegoser (Artemy Egorov)
+
+ This program is free software: you can redistribute it and/or modify
+ it under the terms of the GNU General Public License as published by
+ the Free Software Foundation, either version 3 of the License, or
+ (at your option) any later version.
+
+ This program is distributed in the hope that it will be useful,
+ but WITHOUT ANY WARRANTY; without even the implied warranty of
+ MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ GNU General Public License for more details.
+
+ You should have received a copy of the GNU General Public License
+ along with this program. If not, see <https://www.gnu.org/licenses/>.
+ */
+
 let inputStyle =
   "block px-3 py-1.5 text-base font-normal text-gray-700 dark:text-white bg-white dark:bg-zinc-900 bg-clip-padding border border-solid border-gray-300 rounded-lg transition ease-in-out focus:border-blue-600 focus:outline-none";
 let settingsAddInput = "w-full lg:w-1/4";
